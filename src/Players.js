dfcd0648fe65a3f1b2a7f91e57770b3574e03c4f
januep <83691@student.ue.poznan.pl>
2023-05-15 16:09:15 +0200

    Korzystanie z jsona

--- src/Players.js
+++ src/Players.js
@@ -2,100 +2,9 @@ import React from 'react';
 import { Table } from 'antd';
 import {CustomFlagIconFactory as FlagIconFactory} from 'react-flag-icon-css';
 import { calculateAge } from './utils';
+import players from './players.json';
 
 const FlagIcon = FlagIconFactory(React, { useCssModules: false });
-
-const players = [
-  {
-    "id": 1,
-    "firstName": "Iga",
-    "lastName": "Swiatek",
-    "ranking": 1,
-    "nationality": "PL",
-    "dateOfBirth": "2001-05-31",
-    "height": 176,
-    "weight": 62,
-    "points": 9625
-  },
-  {
-    "id": 2,
-    "firstName": "Aryna",
-    "lastName": "Sabalenka",
-    "ranking": 2,
-    "nationality": "BY",
-    "dateOfBirth": "1998-05-05",
-    "height": 182,
-    "weight": 82,
-    "points": 7881
-  },
-  {
-    "id": 3,
-    "firstName": "Jessica",
-    "lastName": "Pegula",
-    "ranking": 3,
-    "nationality": "US",
-    "dateOfBirth": "1994-02-24",
-    "height": 165,
-    "weight": 68,
-    "points": 5300
-  },
-  {
-    "id": 4,
-    "firstName": "Caroline",
-    "lastName": "Garcia",
-    "ranking": 4,
-    "nationality": "FR",
-    "dateOfBirth": "1993-10-16",
-    "height": 177,
-    "weight": 62,
-    "points": 5025
-  },
-  {
-    "id": 5,
-    "firstName": "Coco",
-    "lastName": "Gauff",
-    "ranking": 5,
-    "nationality": "US",
-    "dateOfBirth": "2004-03-13",
-    "height": 175,
-    "weight": 59,
-    "points": 4345
-  },
-  {
-    "id": 6,
-    "firstName": "Elena",
-    "lastName": "Rybakina",
-    "ranking": 6,
-    "nationality": "KZ",
-    "dateOfBirth": "1999-06-17",
-    "height": 184,
-    "weight": 70,
-    "points": 4195
-  },
-  {
-    "id": 7,
-    "firstName": "Ons",
-    "lastName": "Jabeur",
-    "ranking": 7,
-    "nationality": "TN",
-    "dateOfBirth": "1994-08-28",
-    "height": 167,
-    "weight": 66,
-    "points": 4116
-  },
-  {
-    "id": 8,
-    "firstName": "Maria",
-    "lastName": "Sakkari",
-    "ranking": 8,
-    "nationality": "GR",
-    "dateOfBirth": "1995-07-25",
-    "height": 173,
-    "weight": 64,
-    "points": 3516
-  }
-]
-
 
 const columns = [
   {
@@ -125,14 +34,14 @@ const columns = [
     title: 'Nationality',
     dataIndex: 'nationality',
     render: nationality => <FlagIcon code={nationality.toLowerCase()} />,
-    sorter: (a, b) => a.nationality.localeCompare(b.nationality),
+    responsive: ['md'],
   },
   {
     title: 'Age',
     dataIndex: 'dateOfBirth',
     render: dob => calculateAge(dob),
     sorter: (a, b) => calculateAge(a.dateOfBirth) - calculateAge(b.dateOfBirth),
-    sortDirections: ['descend'],
+    sortDirections: ['descend', 'ascend'],
     responsive: ['md'],
   },
   {
@@ -153,8 +62,13 @@ const columns = [
 
 function Players() {
   return (
-    <Table dataSource={players} columns={columns} rowKey='id' />
-  );
+      <Table 
+        dataSource={players} 
+        columns={columns} 
+        rowKey='id'
+        
+      />
+    );
 }
 
 export default Players;
