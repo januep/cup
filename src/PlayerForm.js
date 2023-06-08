0c458327cc7c9351939e62d14f26ed9b99651c35
januep <83691@student.ue.poznan.pl>
2023-06-08 13:23:20 +0200

    TournamentForm.js better

--- src/PlayerForm.js
+++ src/PlayerForm.js
@@ -1,115 +1,131 @@
-// PlayerForm.js
 import React from "react";
-import { Form, Input, InputNumber, Select, DatePicker, Button } from "antd";
+import {
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  DatePicker,
+  Button,
+  Modal,
+} from "antd";
 
 const { Option } = Select;
 
-const PlayerForm = ({ onFinish }) => (
-  <Form name="manager" onFinish={onFinish} autoComplete="off">
-    <br />
-    <Form.Item
-      name="id"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player id!",
-        },
-      ]}
-    >
-      <InputNumber placeholder="Player ID" />
-    </Form.Item>
+const PlayerForm = ({ onFinish }) => {
+  const handleFinish = (values) => {
+    onFinish(values); // Process the form values here
+    Modal.success({
+      content: "Zawodnik został pomyślnie zapisany!",
+    });
+  };
 
-    <Form.Item
-      name="name"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player name!",
-        },
-      ]}
-    >
-      <Input placeholder="Name" />
-    </Form.Item>
+  return (
+    <Form name="manager" onFinish={handleFinish} autoComplete="off">
+      <br />
+      <Form.Item
+        name="id"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player id!",
+          },
+        ]}
+      >
+        <InputNumber placeholder="Player ID" />
+      </Form.Item>
 
-    <Form.Item
-      name="surname"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player surname!",
-        },
-      ]}
-    >
-      <Input placeholder="Surname" />
-    </Form.Item>
+      <Form.Item
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player name!",
+          },
+        ]}
+      >
+        <Input placeholder="Name" />
+      </Form.Item>
 
-    <Form.Item
-      name="points"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player points!",
-        },
-      ]}
-    >
-      <InputNumber placeholder="Points" />
-    </Form.Item>
-    <Form.Item
-      name="weight"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player weight!",
-        },
-      ]}
-    >
-      <InputNumber placeholder="Weight" />
-    </Form.Item>
-    <Form.Item
-      name="height"
-      rules={[
-        {
-          required: true,
-          message: "Please input the player height!",
-        },
-      ]}
-    >
-      <InputNumber placeholder="Height" />
-    </Form.Item>
-    <Form.Item
-      name="nationality"
-      rules={[
-        {
-          required: true,
-          message: "Please select the player nationality!",
-        },
-      ]}
-    >
-      <Select placeholder="Select a nationality" allowClear>
-        <Select.Option value="PL">Polish</Select.Option>
-        <Select.Option value="US">American</Select.Option>
-        <Select.Option value="DE">German</Select.Option>
-        <Select.Option value="FR">French</Select.Option>
-        <Select.Option value="ES">Spanish</Select.Option>
-      </Select>
-    </Form.Item>
-    <Form.Item
-      name="birthDate"
-      rules={[
-        {
-          required: true,
-          message: "Please select the player birth date!",
-        },
-      ]}
-    >
-      <DatePicker placeholder="Select a birth date" />
-    </Form.Item>
-    <Form.Item>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+      <Form.Item
+        name="surname"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player surname!",
+          },
+        ]}
+      >
+        <Input placeholder="Surname" />
+      </Form.Item>
+
+      <Form.Item
+        name="points"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player points!",
+          },
+        ]}
+      >
+        <InputNumber placeholder="Points" />
+      </Form.Item>
+      <Form.Item
+        name="weight"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player weight!",
+          },
+        ]}
+      >
+        <InputNumber placeholder="Weight" />
+      </Form.Item>
+      <Form.Item
+        name="height"
+        rules={[
+          {
+            required: true,
+            message: "Please input the player height!",
+          },
+        ]}
+      >
+        <InputNumber placeholder="Height" />
+      </Form.Item>
+      <Form.Item
+        name="nationality"
+        rules={[
+          {
+            required: true,
+            message: "Please select the player nationality!",
+          },
+        ]}
+      >
+        <Select placeholder="Select a nationality" allowClear>
+          <Select.Option value="PL">Polish</Select.Option>
+          <Select.Option value="US">American</Select.Option>
+          <Select.Option value="DE">German</Select.Option>
+          <Select.Option value="FR">French</Select.Option>
+          <Select.Option value="ES">Spanish</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="birthDate"
+        rules={[
+          {
+            required: true,
+            message: "Please select the player birth date!",
+          },
+        ]}
+      >
+        <DatePicker placeholder="Select a birth date" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Zapisz
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default PlayerForm;
--- src/TournamentForm.js
+++ src/TournamentForm.js
@@ -1,28 +1,134 @@
-import React from "react";
-import { Form, Input, InputNumber, Select, DatePicker, Button } from "antd";
+import React, { useState } from "react";
+import { Form, Select, Button, Modal, Input } from "antd";
+import players from "./players.json";
 
 const { Option } = Select;
 
-const TournamentForm = ({ onFinish }) => (
-  <Form name="manager" onFinish={onFinish} autoComplete="off">
-    <br />
-    <Form.Item
-      name="id"
-      rules={[
-        {
-          required: true,
-          message: "Please choose player!",
-        },
-      ]}
-    >
-      <InputNumber placeholder="Tournament ID" />
-    </Form.Item>
-    <Form.Item>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+const TournamentForm = ({ onFinish }) => {
+  const [selectedPlayers, setSelectedPlayers] = useState({});
+
+  const handleChange = (value, name) => {
+    setSelectedPlayers((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const filterOptions = (currentValue, option) => {
+    return !Object.values(selectedPlayers).includes(option.value);
+  };
+
+  const handleFinish = (values) => {
+    onFinish(values); // Process the form values here
+    Modal.success({
+      content: "Turniej został pomyślnie zapisany!",
+    });
+  };
+
+  return (
+    <Form name="manager" onFinish={handleFinish} autoComplete="off">
+      <h1>Nazwa turnieju</h1>
+      <Form.Item
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: "Please input the tournament name!",
+          },
+        ]}
+      >
+        <Input placeholder="Tournament name" />
+      </Form.Item>
+      <h1>Pierwszy półfinał</h1>
+      <Form.Item
+        name="semiFinal1Player1"
+        rules={[
+          {
+            required: true,
+            message: "Please choose player 1!",
+          },
+        ]}
+      >
+        <Select
+          placeholder="Wybierz zawodniczkę 1"
+          onChange={(value) => handleChange(value, "semiFinal1Player1")}
+          filterOption={filterOptions}
+        >
+          {players.map((player) => (
+            <Option key={player.id} value={player.lastName}>
+              {player.firstName} {player.lastName}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="semiFinal1Player2"
+        rules={[
+          {
+            required: true,
+            message: "Please choose player 2!",
+          },
+        ]}
+      >
+        <Select
+          placeholder="Wybierz zawodniczkę 2"
+          onChange={(value) => handleChange(value, "semiFinal1Player2")}
+          filterOption={filterOptions}
+        >
+          {players.map((player) => (
+            <Option key={player.id} value={player.lastName}>
+              {player.firstName} {player.lastName}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <h1>Drugi półfinał</h1>
+      <Form.Item
+        name="semiFinal2Player1"
+        rules={[
+          {
+            required: true,
+            message: "Please choose player 3!",
+          },
+        ]}
+      >
+        <Select
+          placeholder="Wybierz zawodniczkę 3"
+          onChange={(value) => handleChange(value, "semiFinal2Player1")}
+          filterOption={filterOptions}
+        >
+          {players.map((player) => (
+            <Option key={player.id} value={player.lastName}>
+              {player.firstName} {player.lastName}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="semiFinal2Player2"
+        rules={[
+          {
+            required: true,
+            message: "Please choose player 4!",
+          },
+        ]}
+      >
+        <Select
+          placeholder="Wybierz zawodniczkę 4"
+          onChange={(value) => handleChange(value, "semiFinal2Player2")}
+          filterOption={filterOptions}
+        >
+          {players.map((player) => (
+            <Option key={player.id} value={player.lastName}>
+              {player.firstName} {player.lastName}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Zapisz turniej
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default TournamentForm;
