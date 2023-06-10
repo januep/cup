5bdc27c5209712ddb1e64beda3894c83b182985e
januep <83691@student.ue.poznan.pl>
2023-06-10 16:17:08 +0200

    dodano współczynnik wygranych

--- src/PlayerProfile.js
+++ src/PlayerProfile.js
@@ -1,70 +1,3 @@
-// import React from "react";
-// import { useParams, Link } from "react-router-dom";
-// import { Descriptions, Avatar, Row, Divider } from "antd";
-// import data from "./players.json";
-// import matches from "./matches.json";
-// import { CustomFlagIconFactory as FlagIconFactory } from "react-flag-icon-css";
-// import { calculateAge } from "./utils";
-// import MatchCard from "./MatchCard";
-//
-// const FlagIcon = FlagIconFactory(React, { useCssModules: false });
-//
-// function PlayerProfile() {
-//   const { id } = useParams(); // Pobieramy ID zawodniczki z parametrów URL
-//   const player = data.find((p) => p.id === Number(id));
-//   // Znajdujemy zawodniczkę o danym ID
-//
-//   if (!player) {
-//     return <p>Zawodniczka nie została znaleziona</p>;
-//   }
-//
-//   const playerMatches = matches
-//     .filter((match) => match.homeId === player.id || match.awayId === player.id)
-//     .map((match) => {
-//       const homePlayer = data.find((p) => p.id === match.homeId);
-//       const awayPlayer = data.find((p) => p.id === match.awayId);
-//       return { ...match, homePlayer, awayPlayer };
-//     });
-//
-//   return (
-//     <div>
-//       <div style={{ display: "flex", justifyContent: "center" }}>
-//         <Avatar size={256} src={require(`./${player.image}`)} />
-//       </div>
-//       <div style={{ display: "flex", justifyContent: "center" }}>
-//         <Divider>{player.firstName + " " + player.lastName}</Divider>
-//       </div>
-//       <Descriptions
-//         bordered
-//         column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-//       >
-//         <Descriptions.Item label="Pozycja">
-//           <Link to="/players">{player.ranking}</Link>
-//         </Descriptions.Item>
-//
-//         <Descriptions.Item label="Narodowość">
-//           <FlagIcon code={player.nationality.toLowerCase()} />
-//         </Descriptions.Item>
-//         <Descriptions.Item label="Data urodzenia">
-//           {player.dateOfBirth} ({calculateAge(player.dateOfBirth)})
-//         </Descriptions.Item>
-//         <Descriptions.Item label="Wzrost">{player.height} cm</Descriptions.Item>
-//         <Descriptions.Item label="Waga">{player.weight} kg</Descriptions.Item>
-//         <Descriptions.Item label="Punkty">{player.points}</Descriptions.Item>
-//       </Descriptions>
-//       <div style={{ display: "flex", justifyContent: "center" }}>
-//         <Divider orientation="left">Mecze zawodniczki</Divider>
-//       </div>
-//       <Row gutter={[16, 16]}>
-//         {playerMatches.map((match) => (
-//           <MatchCard key={match.id} match={match} />
-//         ))}
-//       </Row>
-//     </div>
-//   );
-// }
-//
-// export default PlayerProfile;
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Descriptions, Avatar, Row, Divider, Spin } from "antd";
@@ -77,8 +10,10 @@ const FlagIcon = FlagIconFactory(React, { useCssModules: false });
 function PlayerProfile() {
     const { id } = useParams();
     const [player, setPlayer] = useState(null);
+    const [position, setPosition] = useState(null);
     const [matches, setMatches] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [winRatio, setWinRatio] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8765/api/player/players')
@@ -86,6 +21,13 @@ function PlayerProfile() {
             .then(data => {
                 const playerData = data.find((p) => p.player_id === Number(id));
                 setPlayer(playerData);
+
+                // Sortujemy graczy wg punktów
+                const sortedData = [...data].sort((a, b) => b.points - a.points);
+                // Znajdujemy pozycję gracza
+                const playerPosition = sortedData.findIndex((p) => p.player_id === Number(id)) + 1;
+                setPosition(playerPosition);
+
                 setLoading(false);
             });
     }, [id]);
@@ -94,8 +36,19 @@ function PlayerProfile() {
         fetch('http://localhost:8765/api/match/matches')
             .then(response => response.json())
             .then(data => {
-                const playerMatches = data.filter((match) => match.player1.player_id === Number(id) || match.player2.player_id === Number(id));
+                const playerMatches = data.filter((match) => (match.player1.player_id === Number(id) || match.player2.player_id === Number(id)) && (match.homeScore > 0 || match.awayScore > 0));
                 setMatches(playerMatches);
+
+                const wonMatches = playerMatches.filter((match) => {
+                    if (match.player1.player_id === Number(id)) {
+                        return match.homeScore >= 2;
+                    } else {
+                        return match.awayScore >= 2;
+                    }
+                });
+
+                setWinRatio(wonMatches.length / playerMatches.length);
+
                 setLoading(false);
             });
     }, [id]);
@@ -112,19 +65,19 @@ function PlayerProfile() {
         <div style={{ margin: '0 auto' }}>
             <Divider>{player.first_name + " " + player.last_name}</Divider>
             <Divider><FlagIcon code={player.nationality.toLowerCase()} size={"lg"} /></Divider>
-            <Descriptions
-                bordered
-                column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-            >
+            <Descriptions bordered column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }} >
                 <Descriptions.Item label="Pozycja">
-                    <Link to="/players">{player.ranking}</Link>
+                    <Link to="/players">{position}</Link>
                 </Descriptions.Item>
+                <Descriptions.Item label="Punkty">{player.points}</Descriptions.Item>
+                <Descriptions.Item label="Współczynnik wygranych">{Math.round(winRatio * 100)}%</Descriptions.Item>
                 <Descriptions.Item label="Data urodzenia">
                     {new Date(player.dateOfBirth).toLocaleDateString('pl-PL')} ({calculateAge(new Date(player.dateOfBirth))})
                 </Descriptions.Item>
                 <Descriptions.Item label="Wzrost">{player.height} cm</Descriptions.Item>
                 <Descriptions.Item label="Waga">{player.weight} kg</Descriptions.Item>
-                <Descriptions.Item label="Punkty">{player.points}</Descriptions.Item>
+
+
             </Descriptions>
             <Divider >Mecze zawodniczki:</Divider>
             <Row gutter={16} justify="center">
@@ -137,4 +90,3 @@ function PlayerProfile() {
 }
 
 export default PlayerProfile;
-
